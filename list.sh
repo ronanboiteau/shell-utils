73e2aa09c00686e6898e93d758262301fdd39af7
Ronan Boiteau <ronan.boiteau@epitech.eu>
2017-01-03 23:37:38 +0100

    shebangs

---
+++ list.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## list.sh for list.sh in /home/ronan/rendu/SHL_2016_shell
+## 
+## Made by Ronan Boiteau
+## Login   <ronan.boiteau@epitech.net>
+## 
+## Started on  Tue Jan  3 23:36:47 2017 Ronan Boiteau
+## Last update Tue Jan  3 23:36:48 2017 Ronan Boiteau
+##
